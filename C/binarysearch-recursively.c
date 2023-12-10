e9042912b8f182d97f61b7b194a5165f0f618b0c
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2023-12-10 21:19:32 +0530

    Added comments

--- C/binarysearch-recursively.c
+++ C/binarysearch-recursively.c
@@ -5,10 +5,12 @@ void sort(int a[], int size);
 void binarysearch(int a[], int min, int max, int key);
 int main(void)
 {
+    //Getting input of size of array:
     int n;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
 
+    //Getting user input for elements of array
     int a[n];
     for (int i = 0; i < n; i++)
     {
@@ -16,25 +18,34 @@ int main(void)
         scanf("%d", &a[i]);
     }
 
+    //sorting the array using bubble sort:
     sort(a, n);
 
+
+    //Printing the sorted array:
     printf("Sorted array: ");
     for(int i = 0; i < n; i++)
     {
         printf("%d ", a[i]);
     }
 
+    //Preparing for binary search:
     int key, max, min;
+
+    //Getting input of number to search in the array:
     printf("Enter the number you want to find from the array: ");
     scanf("%d", &key);
     max = n - 1;
     min = 0;
+
+    //Performing binary search:
     binarysearch(a, min, max, key);
     
 }
 
  void sort(int a[], int size)
  {
+    //Bubble sort
     for(int i = 0; i < size; i++)
     {
         for(int j = i + 1; j < size; j++)
@@ -54,14 +65,17 @@ int main(void)
 
  void binarysearch(int a[], int min, int max, int key)
  {
+    //finding the middle element
     int mid = (max + min)/2;
 
+    //break condition:
     if((min == max)&&(a[mid] != key))
     {
         printf("\nNumber not found.\n");
         return;
     }
 
+    //Performing the search:
     if(a[mid] == key)
     {
         printf("Number Found! Number found at %d index. (%d postion)", mid, mid+1);
@@ -75,24 +89,4 @@ int main(void)
         binarysearch(a, min, mid - 1, key);
     }
     return;    
-
-    // int mid = (min + max)/2;
-
-    // if((max == min)&&(a[mid]!=key))
-    // {
-    //     printf("Not found.\n");
-    //     exit(0);
-    // }
-    // else if(a[mid]==key)
-    // {
-    //     printf("Found.\n");
-    //     printf("The element is in the index %d\n", mid);
-    // }
-    // else if (a[mid] < key)
-    // {
-    //     binarysearch(a, mid + 1, max, key);
-    // }
-    // else {
-    //     binarysearch(a, min, mid - 1, key);
-    // }
  }
