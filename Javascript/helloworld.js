4aaab22c4cf92b9175114bd4e9fd0b4d544e30f2
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2023-12-21 22:08:57 +0530

    JS Humble beginnings

---
+++ Javascript/helloworld.js
@@ -0,0 +1,10 @@
+// Hello World
+console.log("Hello, world!");
+
+//Variables
+// JS is dynamically typed - aka dataype is not fixed throught the runtime.
+var a = 15;
+let b = 16;
+//var is used very less. let is more used. var is global while let and const is confined to it's blocks
+//Var can be redeclared while let can't be redeclared
+const pi = 3.14;
